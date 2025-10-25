e1028fbd230ef730d770a03e431af66692c5cf9a
Tim Heessels <timheessels@gmx.com>
2025-10-25 15:52:35 +0200

    Script test

--- owlbeartest/main.js
+++ owlbeartest/main.js
@@ -6,28 +6,40 @@ OBR.onReady(async () => {
   console.log("Enemies Panel plugin ready (OBR SDK v3.1.0)");
 
 
-
-  await OBR.ui.registerPanel({
-    id: `${PLUGIN_ID}/panel`,
-    title: "Enemies",
-    icon: "crossed-swords",
-    show: true,
-    url: "/Durfsheets_Owlbear/owlbeartest/index.html",
-    height: 420
+  OBR.onReady(() => {
+    setupContextMenu();
+    setupInitiativeList(document.querySelector("#initiative-list"));
   });
 
-  // Listen for token changes and update panel
-  OBR.scene.on("token.create", updateTokenList);
-  OBR.scene.on("token.update", updateTokenList);
-  OBR.scene.on("token.delete", updateTokenList);
-
-  // Initial population
-  updateTokenList();
 });
 
-async function updateTokenList() {
-  // Get all tokens on the current map
-  const tokens = await OBR.scene.getTokens();
 
-  console.log("tokens: "+tokens );  
+export function setupInitiativeList(element) {
+  const renderList = (items) => {
+    // Get the name and initiative of any item with
+    // our initiative metadata
+    const initiativeItems = [];
+    for (const item of items) {
+      const metadata = item.metadata[`${ID}/metadata`];
+      if (metadata) {
+        initiativeItems.push({
+          initiative: metadata.initiative,
+          name: item.name,
+        });
+      }
+    }
+    // Sort so the highest initiative value is on top
+    const sortedItems = initiativeItems.sort(
+      (a, b) => parseFloat(b.initiative) - parseFloat(a.initiative)
+    );
+    // Create new list nodes for each initiative item
+    const nodes = [];
+    for (const initiativeItem of sortedItems) {
+      const node = document.createElement("li");
+      node.innerHTML = `${initiativeItem.name} (${initiativeItem.initiative})`;
+      nodes.push(node);
+    }
+    element.replaceChildren(...nodes);
+  };
+  OBR.scene.items.onChange(renderList);
 }
